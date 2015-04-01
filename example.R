d3ee147a935266bc3f1cc436c9fbfeb89723f7ea
Mitchell <mwmitch@transunion.com>
2015-04-01 08:26:08 -0500

    updated example R code

--- example.R
+++ example.R
@@ -1,18 +1,25 @@
 library(devtools)
-install("/Users/assumednormal/Desktop/bartMachine/bartMachine/", local = FALSE)
+install_github("assumednormal/bartMachine/bartMachine", local = FALSE)
 library(bartMachine)
 
+#get data and only use 2 factors
 data(iris)
 iris2 = iris[51:150,]
 iris2$Species = factor(iris2$Species)
+
+#build BART classification model
 bart_machine = build_bart_machine(iris2[ ,1:4], iris2$Species)
+
+##get estimated probabilities
 phat = bart_machine$p_hat_train
+##look at in-sample confusion matrix
 bart_machine$confusion_matrix
 
-.jmethods(bart_machine$java_bart_machine)
 
-for(gibbs in 0:999) {
-    for(tree in 0:49) {
-        cat(.jcall(bart_machine$java_bart_machine, "S", "getDecisionRules", as.integer(gibbs), as.integer(tree)), "\n")    
-    }
-}
+.jmethods(bart_machine$java_bart_machine)
+cat(.jcall(bart_machine$java_bart_machine, "S", "getDecisionRules", as.integer(1), as.integer(1)))
+
+
+iris3 <- iris
+iris3$Species <- factor(iris3$Species)
+colnames(bartMachine:::pre_process_new_data(iris3[1 ,1:4], bart_machine))
